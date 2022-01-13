8c1d016dd91b21f13c728cd9a3765db9f9fb4f44
nkcuong2761 <nkcuong2761@gmail.com>
2022-01-13 17:40:17 -0500

    fix more stuff

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-var timeHoldPress = 0;
-// const spanTime = 1;
-var down = false;
-
 var dict = {
 	'2':"cB1", '3':"dB1", '5':"fB1", '6':"gB1", '7':"aB1", '9':"cB2", '0':"dB2",
 	's':"fB2", 'd':"gB2", 'f':"aB2", 'q':"cW1", 'w':"dW1", 'e':"eW1", 'r':"fW1",
@@ -16,9 +12,8 @@ var idx = new Array();
 function preLoad() {
 	for (key in dict) {
 		var tmp = new Image();
-		var aud = new Audio();
+		var aud = document.getElementById("audio-" + dict[key]);
 		tmp.src = "images/" + dict[key] + "-o.png";
-		aud.src = "Pitch/" + dict[key] + ".wav";
 		duckArr.push(tmp);
 		quackArr.push(aud);
 		idx.push(dict[key]);
@@ -26,42 +21,29 @@ function preLoad() {
 }
 
 function playQuack(note) {
-	note.loop = false;
-	note.load();
+	// note.loop = false;
+	note.currentTime = 0;
 	note.play();
 }
 
 document.addEventListener('keydown', function keyPressed(event) {
+	console.log("keydown");
 	var k = event.key, note = dict[k];
 	var i = idx.indexOf(note);
 	// document.getElementById(note).src = "images/" + note + "-o.png";
 	document.getElementById(note).src = duckArr[i].src;
-    if (down && timeHoldPress>0)
-			return;
 	playQuack(quackArr[i]);
-	timeHoldPress += 1;
-    down = true;
-}, false);
+});
 
 document.addEventListener('keyup', function keyUnpressed(event) {
 	var k = event.key, note = dict[k];
-    down = false;
-    document.getElementById(note).src = "images/" + note + "-l.png";
-    timeHoldPress = 0;
-}, false);
-
-// function keyPressed(event) {
-// 	var k = event.key, note = dict[k], quack = new Audio;
-// 	document.getElementById(note).src = "images/" + note + "-o.png";
-// 	quack.src = "Pitch/" + note + ".wav";
-// 	playQuack(quack);
-// 	timeHoldPress += 1;
-// }
-
-// function keyUnpressed(event) {
-// }
+  document.getElementById(note).src = "images/" + note + "-l.png";
+});
 
 function mouseClicked(id) {
+	if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
+		return
+	// console.log("mouseClicked", id);
 	var i = idx.indexOf(id);
 	// window.alert(duckArr[i].src);
 	document.getElementById(id).src = duckArr[i].src;
@@ -69,5 +51,21 @@ function mouseClicked(id) {
 }
 
 function mouseReleased(id) {
+	if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
+		return
+	// console.log("mouseReleased");
+	document.getElementById(id).src = "images/" + String(id) + "-l.png";
+}
+
+function touched(id) {
+	// console.log("touched", id);
+	var i = idx.indexOf(id);
+	// window.alert(duckArr[i].src);
+	document.getElementById(id).src = duckArr[i].src;
+	playQuack(quackArr[i]);
+}
+
+function touchRelease(id) {
+	// console.log("touchRelease");
 	document.getElementById(id).src = "images/" + String(id) + "-l.png";
 }
